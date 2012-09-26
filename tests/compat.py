7c14c8a3394b5a1fe2713eb93aa6c0acd7297de0
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2012-09-26 13:33:40 +0100

    Raise error on Python 3.0, 3.1 due to limitations of test infrastructure.

--- tests/compat.py
+++ tests/compat.py
@@ -1,7 +1,12 @@
 import sys
 
-if sys.version_info[:2] >= (2, 7):
+_ver = sys.version_info[:2]
+if _ver >= (3, 2):
+    import unittest
+elif _ver <= (2, 6):
+    import unittest2 as unittest
+elif (2, 7) <= _ver < (3, 0):
     import unittest
 else:
-    import unittest2 as unittest
+    raise ValueError('Tests not supported under Python 3.0 and 3.1')
 
