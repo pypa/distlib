325d8e68ff1555ddb67564ee896e62665f270c2f
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2012-10-01 07:09:31 +0100

    Added test_util.

---
+++ tests/test_util.py
@@ -0,0 +1,35 @@
+from compat import unittest
+
+from distlib import DistlibException
+from distlib.util import get_callable
+
+class UtilTestCase(unittest.TestCase):
+    def test_callable_format(self):
+        self.assertIsNone(get_callable('foo.py'))
+        self.assertIsNone(get_callable('foo.py='))
+        self.assertIsNone(get_callable('foo.py=abc'))
+        for spec in ('foo=foo:main', 'foo =foo:main', 'foo= foo:main',
+                     'foo = foo:main'):
+            self.assertEqual(get_callable(spec),
+                             ('foo', 'foo', 'main', []))
+        self.assertEqual(get_callable('foo=foo.bar:main'),
+                             ('foo', 'foo.bar', 'main', []))
+        self.assertEqual(get_callable('foo=foo.bar:main [a]'),
+                             ('foo', 'foo.bar', 'main', ['a']))
+        self.assertEqual(get_callable('foo=foo.bar:main [a=b, c=d,e, f=g]'),
+                             ('foo', 'foo.bar', 'main', ['a=b', 'c=d',
+                                                         'e', 'f=g']))
+        self.assertEqual(get_callable('foo=foo.bar:main [a=9, 9=8,e, f9=g8]'),
+                             ('foo', 'foo.bar', 'main', ['a=9', '9=8',
+                                                         'e', 'f9=g8']))
+        self.assertEqual(get_callable('foo=foo.bar:main[x]'),
+                             ('foo', 'foo.bar', 'main', ['x']))
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x:y')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x [')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x ]')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x []')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x [\]')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x [a=]')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x [a,]')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x [a,,b]')
+        self.assertRaises(DistlibException, get_callable, 'foo=foo.bar:x [a b]')
