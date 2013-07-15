ffecccc20f1f805426a13e1828fe89c93eee2a56
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2013-07-15 11:56:22 +0100

    Changed requirements in tests to conform to recent changes in PEP 426.

--- tests/test_locators.py
+++ tests/test_locators.py
@@ -238,13 +238,13 @@ class LocatorTestCase(unittest.TestCase):
         # Now test with extra in dependency
         locator.clear_cache()
         dummy = make_dist('dummy', '0.1')
-        dummy.metadata.run_requires = ['Jinja2 [i18n]']
+        dummy.metadata.run_requires = [{'requires': ['Jinja2 [i18n]']}]
         dists, problems = finder.find(dummy)
         self.assertFalse(problems)
         actual = sorted([d.name_and_version for d in dists])
         self.assertTrue(actual[0].startswith('Babel ('))
         locator.clear_cache()
-        dummy.metadata.run_requires = ['Jinja2']
+        dummy.metadata.run_requires = [{'requires': ['Jinja2']}]
         dists, problems = finder.find(dummy)
         self.assertFalse(problems)
         actual = sorted([d.name_and_version for d in dists])
@@ -279,7 +279,7 @@ class LocatorTestCase(unittest.TestCase):
         self.assertIsNotNone(d)
         self.assertEqual(d.name_and_version, 'SQLAlchemy (0.6beta3)')
         dist = make_dist('dummy', '0.1')
-        dist.metadata.run_requires = [REQT]
+        dist.metadata.run_requires = [{'requires': [REQT]}]
         dists, problems = finder.find(dist, prereleases=True)
         self.assertFalse(problems)
         actual = sorted(dists, key=lambda o: o.name_and_version)
